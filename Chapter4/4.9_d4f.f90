49d0a2e77ab1ad332198f4d469e6f26ec482b251
IncubatorShokuhou <lh@lasg.iap.ac.cn>
2020-06-28 15:39:40 +0800

    4.9

---
+++ Chapter4/4.9_d4f.f90
@@ -0,0 +1,22 @@
+program main
+    implicit none
+    integer, parameter::N = 1024
+    integer a(N), b(N), c(N)
+    integer i
+ 
+    do i = 1, N
+       a(i) = 0
+       b(i) = i
+       c(i) = i
+    enddo
+ 
+ !$acc kernels
+    do i = 1, N
+       a(i) = b(i) + c(i)
+       b(i) = b(i)*10
+    enddo
+ !$acc end kernels
+    print *, "a(N)=", a(N)
+    print *, "b(N)=", b(N)
+ end program
+ 
