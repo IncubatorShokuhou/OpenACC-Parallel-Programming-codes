fc9de98a438285e47ed9d94caebfe21ac408a340
IncubatorShokuhou <lh@lasg.iap.ac.cn>
2020-06-28 19:09:30 +0800

    prettify

--- Chapter4/4.21_ja2f2.f90
+++ Chapter4/4.21_ja2f2.f90
@@ -1,72 +1,71 @@
 program main
-    implicit none
-    real, parameter::widthy = 2.0, heightx = 1.0
-    real hx, hy
-    integer, parameter::mx = 8191, ny = 1023
-    real(kind=4), allocatable::u0(:, :), u1(:, :)
-    integer maxiter, iter, ix, jy
-    real(kind=4) uerr, errtol
-    real fij, c1, c2, hx2, hy2
-    real tstart, tend
-    real, external::uval
- 
-    maxiter = 100
-    errtol = 0.0
-    hy = widthy/ny
-    hx = heightx/mx
- 
-    allocate (u0(0:mx, 0:ny), u1(0:mx, 0:ny))  !保存二维格点的旧值和新值
- 
-    !Initalize the left/right boundary of u0/u1
-    do ix = 0, mx
-       u0(ix, 0) = uval(ix*hx, 0.0)
-       u0(ix, ny) = uval(ix*hx, ny*hy)
-    enddo
-    u1(0:mx, 0) = u0(0:mx, 0)
-    u1(0:mx, ny) = u0(0:mx, ny)
- 
-    !Initalize the upper/lower boundary of u0/u1
-    do jy = 0, ny
-       u0(0, jy) = uval(0.0, jy*hy)
-       u0(mx, jy) = uval(mx*hx, jy*hy)
-    enddo
-    u1(0, 0:ny) = u0(0, 0:ny)
-    u1(mx, 0:ny) = u0(mx, 0:ny)
- 
-    !Initalize the interior point of u0
-    u0(1:mx - 1, 1:ny - 1) = 0.0
- 
-    fij = -4.
-    c1 = hx*hx*hy*hy
-    c2 = 1./(2.*(hx*hx + hy*hy))
-    hx2 = hx*hx
-    hy2 = hy*hy
-    call cpu_time(tstart)
-    ! main iterationsl
-    do iter = 1, maxiter, 2
-       !$acc kernels copy(u0) copyin(u1)
-       do jy = 1, ny - 1
-          do ix = 1, mx - 1
-             u1(ix, jy) = (fij*c1 + hy2*(u0(ix - 1, jy) + u0(ix + 1, jy)) + hx2*(u0(ix, jy - 1) + u0(ix, jy + 1)))*c2
-          enddo
-       enddo
-       !$acc end kernels
-       !$acc kernels copy(u0) copyin(u1)
-       do jy = 1, ny - 1
-          do ix = 1, mx - 1
-             u0(ix, jy) = (fij*c1 + hy2*(u1(ix - 1, jy) + u1(ix + 1, jy)) + hx2*(u1(ix, jy - 1) + u1(ix, jy + 1)))*c2
-             ! uerr = max(uerr,abs(u0(ix, jy)-u1(ix, jy)))
-          enddo
-       enddo
-       !$acc end kernels
-    enddo
-    call cpu_time(tend)
-    print *, "ElapsedTime =", tend - tstart, " seconds"
-    deallocate (u0, u1)
- end program
- !========================
- function uval(x, y)
-    real, intent(in)::x, y
-    uval = x*x + y*y
- end function
- 
+   implicit none
+   real, parameter::widthy = 2.0, heightx = 1.0
+   real hx, hy
+   integer, parameter::mx = 8191, ny = 1023
+   real(kind=4), allocatable::u0(:, :), u1(:, :)
+   integer maxiter, iter, ix, jy
+   real(kind=4) uerr, errtol
+   real fij, c1, c2, hx2, hy2
+   real tstart, tend
+   real, external::uval
+
+   maxiter = 100
+   errtol = 0.0
+   hy = widthy/ny
+   hx = heightx/mx
+
+   allocate (u0(0:mx, 0:ny), u1(0:mx, 0:ny))  !保存二维格点的旧值和新值
+
+   !Initalize the left/right boundary of u0/u1
+   do ix = 0, mx
+      u0(ix, 0) = uval(ix*hx, 0.0)
+      u0(ix, ny) = uval(ix*hx, ny*hy)
+   enddo
+   u1(0:mx, 0) = u0(0:mx, 0)
+   u1(0:mx, ny) = u0(0:mx, ny)
+
+   !Initalize the upper/lower boundary of u0/u1
+   do jy = 0, ny
+      u0(0, jy) = uval(0.0, jy*hy)
+      u0(mx, jy) = uval(mx*hx, jy*hy)
+   enddo
+   u1(0, 0:ny) = u0(0, 0:ny)
+   u1(mx, 0:ny) = u0(mx, 0:ny)
+
+   !Initalize the interior point of u0
+   u0(1:mx - 1, 1:ny - 1) = 0.0
+
+   fij = -4.
+   c1 = hx*hx*hy*hy
+   c2 = 1./(2.*(hx*hx + hy*hy))
+   hx2 = hx*hx
+   hy2 = hy*hy
+   call cpu_time(tstart)
+   ! main iterationsl
+   do iter = 1, maxiter, 2
+      !$acc kernels copy(u0) copyin(u1)
+      do jy = 1, ny - 1
+         do ix = 1, mx - 1
+            u1(ix, jy) = (fij*c1 + hy2*(u0(ix - 1, jy) + u0(ix + 1, jy)) + hx2*(u0(ix, jy - 1) + u0(ix, jy + 1)))*c2
+         enddo
+      enddo
+      !$acc end kernels
+      !$acc kernels copy(u0) copyin(u1)
+      do jy = 1, ny - 1
+         do ix = 1, mx - 1
+            u0(ix, jy) = (fij*c1 + hy2*(u1(ix - 1, jy) + u1(ix + 1, jy)) + hx2*(u1(ix, jy - 1) + u1(ix, jy + 1)))*c2
+            ! uerr = max(uerr,abs(u0(ix, jy)-u1(ix, jy)))
+         enddo
+      enddo
+      !$acc end kernels
+   enddo
+   call cpu_time(tend)
+   print *, "ElapsedTime =", tend - tstart, " seconds"
+   deallocate (u0, u1)
+end program
+!========================
+function uval(x, y)
+   real, intent(in)::x, y
+   uval = x*x + y*y
+end function
--- Chapter4/4.23_d8f.f90
+++ Chapter4/4.23_d8f.f90
@@ -1,7 +1,32 @@
 program main
-    implicit none
-    integer, parameter::N =256
-    integer a(N), b(N),i
-    a(:)=0
-    b(:)=0 
-    !$acc
+   implicit none
+   integer, parameter::N = 256
+   integer a(N), b(N), i
+   a(:) = 0
+   b(:) = 0
+   !$acc kernels
+   do i = 1, N
+      a(i) = a(i) + i
+      b(i) = a(i)
+   enddo
+   !$acc end kernels
+   !$acc kernels
+   do i = 1, N
+      a(i) = a(i) - i
+      b(i) = a(i)
+   enddo
+   !$acc end kernels
+   a(:) = 0
+   b(:) = 0
+   !$acc kernels
+   do i = 1, N
+      a(i) = a(i) + i
+      b(i) = a(i)
+   enddo
+   do i = 1, N
+      a(i) = a(i) - i
+      b(i) = a(i)
+   enddo
+   !$acc end kernels
+   print *, "b(N)=", b(N)
+end program
