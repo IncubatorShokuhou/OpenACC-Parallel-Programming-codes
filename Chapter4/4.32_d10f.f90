cfebe6e61e0d62c55131b8a69d254f893cf20526
IncubatorShokuhou <lh@lasg.iap.ac.cn>
2020-09-16 15:03:34 +0800

    4.32

--- Chapter4/4.32_d10f.f90
+++ Chapter4/4.32_d10f.f90
@@ -10,10 +10,10 @@ program main
     !$acc enter data copyin(v1(1:length))
     !$acc parallel loop present(v1(1:length))
     do idx = 1, length
-        v1(idx) = v1(idx) + idx    !设备上v1(1)=1, host上v1(1)=0
+        v1(idx) = v1(idx) + idx    !设备上v1(1)=1, host上v1(1)=0   
     enddo
 
-    !$acc update host(v1(1:1))
+    !$acc update host(v1(1:1))     !可以指定不连续的子数组
     print*, "v1(1) = ", v1(1)      !设备上v1(1)=1, update到host上,  host上v1(1)=1
 
     !$acc parallel loop present(v1(1:length))
